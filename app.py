a49ecbede96906276b39123666de0671b00f08d8
Tael Ling Lin <taellinglin@gmail.com>
2022-03-29 20:33:45 -0500

    Tael Ling Lin

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from typing import Any
 from direct.showbase.ShowBase import ShowBase
 from direct.actor.Actor import Actor
 from panda3d.core import *
@@ -13,22 +12,26 @@ class App(ShowBase):
         dlnp.setHpr(0, -60, 0)
         render.setLight(dlnp)
         alight = AmbientLight('alight')
-        alight.setColor((1, 1, 1, 0.25))
+        alight.setColor((1, 1, 1, 0.125))
         alnp = render.attachNewNode(alight)
         render.setLight(alnp)
         # Load the environment model.
         model = loader.loadModel('models/Ratcoon.bam')
         self.actor = Actor(model, {
-            'walk': 'models/Ratcoon_walk.bam',
-            'attack': 'models/Ratcoon_attack.bam',
+            'walk': 'models/Ratcoon_actions.bam',
+            'attack': 'models/Ratcoon_actions.bam'
+            
         })
         #self.scene = self.loader.loadModel("models/Ratcoon_actions.bam")
         # Reparent the model to render.
         
         
+        
         self.actor.loop('walk')
+
         #self.actor.stop('attack')
         self.actor.reparentTo(self.render)
+        print(str(self.actor.get_anim_names()))
         print(str(self.actor.get_current_anim()))
         
         
